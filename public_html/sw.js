2c77f45cc119d5446e346473f004faaa96459589
HRoknizadeh <h.roknizadeh@it-p.de>
2017-12-09 12:12:19 +0100

    first commit

---
+++ public_html/sw.js
@@ -0,0 +1,98 @@
+/* global caches, self */
+
+const staticAssets = [
+    './',
+    './app.js'
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open('oraman-static');
+    console.log('install');
+    console.log(cache);
+    cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkFirst(req));
+    }
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open('news-dynamic');
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (e) {
+        return await cache.match(req);
+    }
+
+}
+//////////////////////////////
+
+/* eslint-enable max-len */
+const applicationServerPublicKey = 'BIkKyxwXjMnifoCuwyW_SlAu_k9ZrcPjhE6lUxRo3CpUme1ifrU3Jj9KkROQXNCTW9ecgG_lVtrmoiazxIwJ1ow';
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://developers.google.com/web/')
+  );
+});
+
+self.addEventListener('pushsubscriptionchange', function(event) {
+  console.log('[Service Worker]: \'pushsubscriptionchange\' event fired.');
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+  event.waitUntil(
+    self.registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: applicationServerKey
+    })
+    .then(function(newSubscription) {
+      // TODO: Send to application server
+      console.log('[Service Worker] New subscription: ', newSubscription);
+    })
+  );
+});
